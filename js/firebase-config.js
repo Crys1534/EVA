b516bc8d77f571b4e71d3a1ad51bd95049b40b2e
Crystal <crystalsnow1534@gmail.com>
2026-05-11 00:04:49 -0600

    Alpha 0.13

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// Importamos solo lo que necesitamos para ahorrar espacio (Modular SDK)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnX__R6A6lQw_KuzZxqHRkzZyj10S_4wU",
   authDomain: "aula-virtual-79e4d.firebaseapp.com",
+  databaseURL: "https://aula-virtual-79e4d-default-rtdb.firebaseio.com",
   projectId: "aula-virtual-79e4d",
   storageBucket: "aula-virtual-79e4d.firebasestorage.app",
   messagingSenderId: "384731632952",
@@ -10,6 +13,6 @@ const firebaseConfig = {
   measurementId: "G-SR9D2CJX40"
 };
 
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+export const db = getFirestore(app);
